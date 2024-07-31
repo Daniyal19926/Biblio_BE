482ef083a3029658711ac8f84ac67f2f0d001f1d
Daniyal <you@example.com>
2024-07-31 10:10:28 +0200

    finished books route

--- routes/books.ts
+++ routes/books.ts
@@ -1,8 +1,76 @@
 import express from "express";
 import { PrismaClient } from "@prisma/client";
+import { validate } from "../schemas/Book";
 const router = express.Router();
 const prisma = new PrismaClient();
-router.get("/", (req, res) => {
-  res.send("still working");
+router.get("/", async (req, res) => {
+  const books = await prisma.book.findMany({ include: { category: true } });
+  return res.status(200).send(books);
 });
+router.get("/:id", async (req, res) => {
+  const book = await prisma.book.findFirst({
+    where: { id: req.params.id },
+    include: { category: true },
+  });
+  if (!book) return res.status(400).send("book with given id not found");
+  return res.status(200).send(book);
+});
+router.put("/:id", async (req, res) => {
+  const book = await prisma.book.findFirst({ where: { id: req.params.id } });
+  if (!book) return res.status(400).send("book with given id not found");
+  const validation = validate(req.body);
+  if (!validation.success)
+    return res.status(400).send(validation.error.issues[0].message);
+  const category = await prisma.category.findFirst({
+    where: { id: req.body.categoryId },
+  });
+  if (!category) return res.status(400).send("category not found");
+  const updatedBook = await prisma.book.update({
+    where: { id: req.params.id },
+    data: {
+      author: req.body.author,
+      title: req.body.title,
+      nbrPages: req.body.nbrPages,
+      type: req.body.type,
+      categoryId: req.body.categoryId,
+    },
+    include: { category: true },
+  });
+  return res.status(200).send(updatedBook);
+});
+router.post("/", async (req, res) => {
+  const validation = validate(req.body);
+  if (!validation.success) {
+    return res.status(400).send(validation.error.issues[0].message);
+  }
+  const category = await prisma.category.findFirst({
+    where: { id: req.body.categoryId },
+  });
+  if (!category) return res.status(400).send("category does not exist");
+  const book = await prisma.book.create({
+    data: {
+      author: req.body.author,
+      title: req.body.title,
+      nbrPages: req.body.nbrPages,
+      type: req.body.type,
+      categoryId: req.body.categoryId,
+    },
+    include: {
+      category: true,
+    },
+  });
+  return res.status(201).send(book);
+});
+
+router.delete("/:id", async (req, res) => {
+  const book = await prisma.book.findFirst({ where: { id: req.params.id } });
+  if (!book) return res.status(400).send("book with given id not found");
+
+  const deletedBook = await prisma.book.delete({
+    where: { id: req.params.id },
+    include: { category: true },
+  });
+  return res.status(200).send(deletedBook);
+});
+
 export default router;
